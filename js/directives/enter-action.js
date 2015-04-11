1d7c04498ede26761edf56409e36afcb4d03d2f4
haggis22 <dshell@gmail.com>
2015-04-11 16:27:44 -0500

    adding directives for focusing on elements and accepting enter to trigger

---
+++ js/directives/enter-action.js
@@ -0,0 +1,21 @@
+"use strict";
+
+(function (app) {
+
+    app.directive('enterAction', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.enterAction);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
+
+})(angular.module('CurseApp'));
+	
+
---
+++ js/directives/focus-me.js
@@ -0,0 +1,22 @@
+"use strict";
+
+(function (app) {
+
+    app.directive('focusMe', function ($timeout) {
+        return {
+            link: function (scope, element, attrs) {
+                scope.$watch(attrs.focusMe, function (value) {
+                    if (value === true) {
+                        // the timeout will give any container time to appear
+                        $timeout(function() {
+                            element[0].focus();
+                        }, 200);
+                    }
+                });
+            }
+        };
+    });
+
+})(angular.module('CurseApp'));
+	
+
