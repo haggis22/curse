94e901f1e598e5e25be08cc962a4f6bd6ae412a0
haggis22 <dshell@gmail.com>
2016-01-09 20:48:43 -0600

    Now accepts a string rather than an error object to easily show a given message

--- client/common/services/errorService.js
+++ client/common/services/errorService.js
@@ -10,36 +10,44 @@
 
                 var response =
                 {
+                    display: true,
                     header: header,
                     httpStatusCode: errorObject.status,
                     errors: []
                 };
 
-                // In Chrome/Firefox, certain types of error will have a status of 0: Aborted requests, CORS errors,
-                // invalid server names, etc.  Working a way to see what is actually in there
-                if (errorObject.status === 0) {
-
-                    var x = 0;
-
+                if (typeof errorObject == 'string')
+                {
+                    response.errors.push(errorObject);
                 }
-                else {
+                else
+                {
 
-                    if (errorObject.data) {
-
-                        response.responseCode = errorObject.data["error_code"];
-
-                        if (errorObject.data.error_message != null) {
-                            response.errors.push(errorObject.data.error_message);
-                        }
-                        if (errorObject.data.error != null) {
-                            response.errors.push(errorObject.data.error);
-                        }
+                    // In Chrome/Firefox, certain types of error will have a status of 0: Aborted requests, CORS errors,
+                    // invalid server names, etc.  Working a way to see what is actually in there
+                    if (errorObject.status === 0) {
 
                     }
-                    else if (errorObject.description) {
-                        response.errors.push(errorObject.description);
-                    }
+                    else {
 
+                        if (errorObject.data) {
+
+                            response.responseCode = errorObject.data["error_code"];
+
+                            if (errorObject.data.error_message != null) {
+                                response.errors.push(errorObject.data.error_message);
+                            }
+                            if (errorObject.data.error != null) {
+                                response.errors.push(errorObject.data.error);
+                            }
+
+                        }
+                        else if (errorObject.description) {
+                            response.errors.push(errorObject.description);
+                        }
+
+
+                    }
 
                 }
 
@@ -47,7 +55,6 @@
                     response.errors.push("No details available.");
                 }
 
-                response.display = true;
                 return response;
 
 
